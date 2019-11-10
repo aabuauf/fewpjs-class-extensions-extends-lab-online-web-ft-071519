6a79b9c88a181333de21e9fe14df30c22e2fcd43
Ahmed Abououf <aabuauf@yahoo.com>
2019-11-10 12:30:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 // Your code here
 class Polygon {
+<<<<<<< HEAD
   constructor (array){
+=======
+  constractor (array){
+>>>>>>> 5c4c9c4cf05a9a924012025f7cad9059c5361d79
     this.array=array
   }
   
   
+<<<<<<< HEAD
   get countSides() {
     return this.array.length
   }
@@ -40,3 +45,17 @@ class Triangle extends Polygon{
       return this.array[0]* this.array[0]
     }
   }
+=======
+  get countSides (array){
+    return this.array.length
+  }
+  
+  get perimeter (array){
+    let per = 0
+    for (let i =0; i< this.array.length;i++){
+      per= per + array[i]
+    }
+    return per
+  }
+}
+>>>>>>> 5c4c9c4cf05a9a924012025f7cad9059c5361d79
